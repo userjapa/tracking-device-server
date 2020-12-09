627c95774461539c49688c20f65032b46e797ca5
usuarioJapa <willian.hsimabukuro@gmail.com>
2020-12-08 23:48:44 -0300

    update: init

---
+++ index.js
@@ -0,0 +1,42 @@
+const http       = require('http'),
+      socketIo   = require('socket.io'),
+      express    = require('express'),
+      bodyParser = require('body-parser'),
+      cors       = require('cors')
+
+const PORT = process.env.PORT || 8080
+
+const app        = express(),
+      httpServer = http.createServer(app),
+      io         = socketIo(httpServer, { path: '/socket' })
+
+app.use(bodyParser.text())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: ['Content-Type']
+}))
+
+app.get('/', (req, res) => {
+  res.send('Tracking Device Server')
+})
+
+app.post('/tracking', (req, res) => {
+  console.log(req.body)
+
+  const [ latitude, longitude ] = req.body.split(';')
+
+  io.emit('tracking', {
+    latitude,
+    longitude
+  })
+
+  res.send()
+})
+
+app.use('*', (req, res) => {
+  res.status(404).send('Not found')
+})
+
+httpServer.listen(PORT, () => console.log(`Running at port ${PORT}`))
